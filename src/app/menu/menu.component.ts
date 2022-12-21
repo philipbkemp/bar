2eee97d9f2b761209ab3eee41b87bdbd77dd2370
Phil Kemp <boondock.dragon@gmail.com>
2022-12-21 16:18:10 +0100

    Stop inactive user going to menu page

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TranslocoService } from "@ngneat/transloco";
+import { Router } from "@angular/router";
 
 import { ApiService } from "@app/services/api.service";
+import { CookieService } from "@app/services/cookie.service";
+import { AppConfigService } from "@app/services/config.service";
 import { Subscription } from "rxjs";
 import { BarResponse } from "@app/interfaces/barResponse.interface";
 
@@ -15,14 +18,48 @@ import { MessageService } from "primeng/api";
 })
 export class MenuComponent implements OnInit, OnDestroy {
 
+	sub_patronStatus!:Subscription;
+
+	isLoading: boolean = true;
+	isLoadingError: boolean = false;
+
 	constructor(
+		private config: AppConfigService,
+		private cookie: CookieService,
+		private api: ApiService,
+		private router: Router
 	) {
 	}
 
 	ngOnInit() {
+		let knownPatron:any = this.cookie.cookieGet( this.config.cookiePrefix + ".patron");
+		if ( knownPatron ) {
+			knownPatron = JSON.parse(knownPatron);
+			if ( this.sub_patronStatus ) { this.sub_patronStatus.unsubscribe(); }
+			this.sub_patronStatus = this.api.isPatronValid(knownPatron).subscribe((r:BarResponse)=>{
+				if ( r.success && ! r.err ) {
+					this.isLoading = false;
+					if ( ! r.payload ) {
+						this.router.navigate(['/']);
+					} else {
+						this.loadDrinks();
+					}
+				} else {
+					this.isLoadingError = true;
+					this.isLoading = false;
+				}
+			});
+		} else {
+			this.isLoading = false;
+		}
 	}
 
 	ngOnDestroy() {
+		if ( this.sub_patronStatus ) { this.sub_patronStatus.unsubscribe(); }
+	}
+
+	loadDrinks() {
+		console.log("LOADING DRINKS");
 	}
 
 }
